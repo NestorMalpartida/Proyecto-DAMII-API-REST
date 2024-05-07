1f7a0b72b07f0b0d3ab911b7a677699ae6c8f175
NestorMalpartida <nestor.malpartida.04.97@hotmail.com>
2024-05-07 12:50:03 -0500

    API

--- src/main/java/com/proyecto/restaurant/ProyectoDamiiApiRestApplication.java
+++ src/main/java/com/proyecto/restaurant/ProyectoDamiiApiRestApplication.java
@@ -2,6 +2,10 @@ package com.proyecto.restaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProyectoDamiiApiRestApplication {
@@ -10,4 +14,17 @@ public class ProyectoDamiiApiRestApplication {
 		SpringApplication.run(ProyectoDamiiApiRestApplication.class, args);
 	}
 
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
 }
