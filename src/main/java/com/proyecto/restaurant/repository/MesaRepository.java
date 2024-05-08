d8313e6d684ef268e25004fb32c7d4d0a9572285
NestorMalpartida <nestor.malpartida.04.97@hotmail.com>
2024-05-08 11:02:16 -0500

    ACT

--- src/main/java/com/proyecto/restaurant/repository/MesaRepository.java
+++ src/main/java/com/proyecto/restaurant/repository/MesaRepository.java
@@ -9,6 +9,6 @@ import com.proyecto.restaurant.entity.Mesa;
 
 public interface MesaRepository extends JpaRepository<Mesa, Integer> {
 
-	@Query("Select m form Mesa m inner join Sede s on s.codigo = m.sede.codigo where s.codigo = ?1")
+	@Query("Select m from Mesa m inner join Sede s on s.codigo = m.sede.codigo where s.codigo = ?1")
 	public List<Mesa> findMesaBySede(Integer codigo);
 }
