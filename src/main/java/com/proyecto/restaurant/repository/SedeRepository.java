80a574ebff4cab0f30b2a956ec731b41cd3bcc3d
NestorMalpartida <nestor.malpartida.04.97@hotmail.com>
2024-05-08 11:07:00 -0500

    ACT

--- src/main/java/com/proyecto/restaurant/repository/SedeRepository.java
+++ src/main/java/com/proyecto/restaurant/repository/SedeRepository.java
@@ -7,6 +7,6 @@ import com.proyecto.restaurant.entity.Sede;
 
 public interface SedeRepository extends JpaRepository<Sede, Integer> {
 
-	@Query("Select s from Sede where s.usuario.codigo = ?1")
+	@Query("Select s from Sede s where s.usuario.codigo = ?1")
 	public Sede findSedeByUsuario(Integer codigo);
 }
